5b925fa6d3d497ed6a9bb58cedbd429d4fa3588c
= <=>
2021-02-28 19:55:33 +0300

    api projesi eklendi

---
+++ Api/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using OnlineTicariOtomasyon.Models.Sınıflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Api.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Context c = new Context();
+        public List<Urun> Get()
+        {
+            var data = c.Uruns.Where(x=>x.Durum==true).ToList();
+             
+                return data;
+        }
+    }
+}
